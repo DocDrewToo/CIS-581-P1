0bf4414e65c233d9545dcb389be7264cb8d6d72d
Doc Drew <docdrew@umich.edu>
2022-02-16 19:07:34 -0500

    fit - least squares

--- app/twards-data-science.py
+++ app/twards-data-science.py
@@ -1,16 +1,31 @@
+from re import X
 import numpy
 from scipy.linalg import lstsq
 from sklearn.preprocessing import PolynomialFeatures
 
-def fit(x, y, solver, _degree):
+
+def x_data_in_polynomial_matrix(x_data, _degree):
+    x_reshaped = x_data.reshape(-1,1)
+    polynomial_features = PolynomialFeatures(degree=_degree)
+    X = polynomial_features.fit_transform(x_reshaped)
+    return X
+
+
+def transform(x_data):
+    
+
+
+def fit(X, y, solver):
     # X : array-like of shape (n_samples, n_features)
     # y : array-like of shape (n_samples, 1)
     if solver == 'lstsq':
-        x_reshaped = x_data.reshape(-1,1)
-        polynomial_features = PolynomialFeatures(degree=_degree)
-        X = polynomial_features.fit_transform(x_reshaped)
-        X = np.c_[np.ones((X.shape[0], 1)), X]
+        coefecients, residues, rank, singular = lstsq(X,y)
+    return coefecients, residues, rank, singular 
 
+
+def predict(coefecients, X):
+    x_predictions = X.dot(coefecients)
+    return x_predictions
 
 
 def seperate_data_to_x_y_arrays(input_data_file_name):    
@@ -30,4 +45,9 @@ def seperate_data_to_x_y_arrays(input_data_file_name):
 data = "data/test.txt"
 # data = "data/deficit_train.dat"
 x_data, y_data = seperate_data_to_x_y_arrays(data)
-fit(x_data, y_data, "lstsq", degree=2)
+degree = 2
+transform(x_data)
+x_matrix = x_data_in_polynomial_matrix(x_data, degree)
+coeficients, _, _, _ = fit(x_matrix, y_data, "lstsq")
+hypothesis = predict(coeficients, x_matrix)
+print(hypothesis)
